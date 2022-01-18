4e065fe7d90c50dd757d8313c35a9e2b52bfab9a
WagnerChr <wagnerckv@gmail.com>
2022-01-18 11:05:52 -0300

    testando

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-let num = 0
+let count = 0
 const counter = document.getElementById("counter")
 
 function increment() {
-    num++
-    counter.innerText = num;
+    count++
+    counter.innerText = count;
 }
 
 
 let saveP = document.getElementById("p-save")
 
 function save() {
-    let counterStr = num + "-"
-    saveP.innerText += counterStr
+    let counterStr = count + " - "
+// innerText apenas pega "Legíveis". textContent, neste caso, é melhor por pegar tudo do elemento incluindo os " "
+    saveP.textContent += counterStr
+    count = 0;
+    counter.innerText = count;
+    // aparece a poha do código
 }
