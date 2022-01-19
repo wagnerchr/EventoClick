9fdeab14fc2b78ad05287289b68bca53fa7693da
WagnerChr <wagnerckv@gmail.com>
2022-01-19 10:27:23 -0300

    Agora com função de apagar

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 let count = 0
 const counter = document.getElementById("counter")
 
-function increment() {
-    count++
-    counter.innerText = count;
-}
+// Acrescenta os números
+    function increment() {
+        count++
+        counter.innerText = count;
+    }
+
+// Corrigir/ Apagar
+
+    function erase() {
+        count--
+    if(count < 0) {
+        count = 0
+    } else {count = count}
+        counter.innerText = count;
+    }
 
 
 let saveP = document.getElementById("p-save")
 
-function save() {
-    let counterStr = count + " - "
-// innerText apenas pega "Legíveis". textContent, neste caso, é melhor por pegar tudo do elemento incluindo os " "
-    saveP.textContent += counterStr
-    count = 0;
-    counter.innerText = count;
-    
+// Salva os números
+    function save() {
+        let counterStr = count + " - "
+    // innerText apenas pega "Legíveis". textContent, neste caso, é melhor por pegar tudo do elemento incluindo os " "
+        saveP.textContent += counterStr
+        count = 0;
+        counter.innerText = count;
+
+
 }
