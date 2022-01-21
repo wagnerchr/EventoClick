4395856e4573492753d48b9044bdeae0ddeef2b7
WagnerChr <wagnerckv@gmail.com>
2022-01-21 19:15:55 -0300

    Almost ready, just need to add random cards

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Contador 
 let count = 0
 const counter = document.getElementById("counter")
 
@@ -27,6 +28,50 @@ let saveP = document.getElementById("p-save")
         saveP.textContent += counterStr
         count = 0;
         counter.innerText = count;
-
-
 }
+
+//Blackjack
+
+let firstCard = 10
+let secondCard = 11
+let cards = [firstCard, secondCard] // Array para armazenar as cartas
+let sum = firstCard + secondCard
+
+
+messageGame = document.getElementById("message-game");
+sumEl = document.getElementById("message-sum");
+cardsEl = document.getElementById("message-cards");
+result = document.getElementById("message-result");
+
+// Função startGame ainda faz sentido no html
+function startGame() {
+    renderGame()
+}
+// foi renomeado de "renderGame" pois não faz sentido uma função "começar jogo" ser chamada novamente com o jogo já começado
+ 
+function renderGame() {
+    messageGame.textContent = "Nice, lesgo"
+    cardsEl.textContent = "Cards: " // Este serve como um "reset" para não repetir a primeira e segunda carta após comprar a terceira. A primeira e a segunda são escritas novamente junto da terceira, sem repeti-lás
+    for (card in cards) {
+        cardsEl.textContent += cards[card] + " "
+    }
+    sumEl.textContent = ("Sum: "+ sum)
+
+    if(sum < 21) {
+        result.textContent = "Okay, do you wanna draw another card?"
+    } else if (sum === 21) {
+        result.textContent = "Woah, you've got a BlackJack :))"
+    } else {
+        result.textContent = "You're out of the game! :("
+    }
+}
+
+// Função comprar nova carta
+
+function drawCard() {
+    let newCard = 2
+    cards.push(newCard) // Adicionando ao array
+    sum += newCard      // Adicionando à soma
+    renderGame()
+}
+
